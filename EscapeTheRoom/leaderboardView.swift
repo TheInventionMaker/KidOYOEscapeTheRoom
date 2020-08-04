9163909686611baa3b22466b1ea13a5b314c4142
TheInventionMaker <nagswhiz@gmail.com>
2020-08-04 17:40:15 -0400

    Fixed for tiny screens and the leaderboard has been improved.

--- EscapeTheRoom/leaderboardView.swift
+++ EscapeTheRoom/leaderboardView.swift
@@ -21,31 +21,42 @@ struct leaderboardView: View {
                         .fontWeight(.bold)
                         .padding()
                     ForEach(0..<10){ n in
-                        HStack{
-                           
-                            Text("JRamo\(n + 1)")
-                                .font(.title)
-                                .fontWeight(.bold)
-                                .padding()
-                                
-                            Spacer()
-                        }
                         ZStack{
-                            Rectangle()
-                                .frame(height: 5, alignment: .center)
-                                .frame(maxWidth: 375)
-                                .overlay(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
-                            HStack{
-                              
-                                Spacer()
-                                Rectangle()
-                                    
-                                    .frame(width: geometry.size.width * CGFloat(precentages[n]), height: 5, alignment: .center)
-                                    
-                                    .foregroundColor(colorScheme == .dark ? .black : .white)
+                            
+                            VStack(spacing: 0){
+                                HStack{
+                                   Text("\(n + 1). ")
+                                    .foregroundColor(.red)
+                                    .font(.title2)
+                                    .fontWeight(.medium)
+                                    Text("JRamo\(n + 1)")
+                                        .font(.title2)
+                                        .fontWeight(.light)
+                                        
+                                        
+                                    Spacer()
+                                    Text("\(100 - Int(precentages[n] * 100))% Complete")
+                                }.padding([.top, .leading, .trailing])
+                                ZStack{
+                                   
+                                    Rectangle()
+                                        .frame(height: 5, alignment: .center)
+                                        .frame(maxWidth: .infinity)
+                                        .overlay(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
+                                        .cornerRadius(5)
+                                        .padding(.horizontal)
+                                    HStack{
+                                      
+                                        Spacer()
+                                        Rectangle()
+                                            
+                                            .frame(width: geometry.size.width * CGFloat(precentages[n]), height: 20, alignment: .center)
+                                            
+                                            .foregroundColor(colorScheme == .dark ? .black : .white)
+                                    }
+                                }
                             }
                         }
-                           
                            
                         
                     }
